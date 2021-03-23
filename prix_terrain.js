ceda6dc9e275bb8988f77e6156782bb6f8667348
LucasZuk <lucas.zukervar@gmail.com>
2021-03-23 14:26:05 +0100

    HTML & CSS Ok

--- prix_terrain.js
+++ prix_terrain.js
@@ -1,14 +1,25 @@
 /* Connexion avec l'API des codes postaux api.gouv.fr */
 
-var codePostal = '01390';
-
-fetch(`https://apicarto.ign.fr/api/codes-postaux/communes/${codePostal}`)
-	.then(response => response.json())
-	.then(data =>
-		{
-			for (const commune of data)
+	function getCodePostal(codePostal)
+	{
+		fetch(`https://apicarto.ign.fr/api/codes-postaux/communes/${codePostal}`)
+		.then(response => response.json())
+		.then(data =>
 			{
-				console.log(commune.nomCommune);
-			}
-		})
-	.catch(console.log('Une erreur est survenue..'));
+				var listeVille = '<option value="">Trouver une ville</option>';
+
+				for (const commune of data)
+				{
+					listeVille += `<option value = "${commune.nomCommune}">${commune.nomCommune}</option>`;
+				}
+				ville.innerHTML = listeVille;
+			})
+		.catch(console.log('Une erreur est survenue..'));
+	}
+
+
+/* Récupération éléments du DOM */
+
+var codePostalBox = document.getElementById("codePostal");
+var ville = document.getElementById("ville");
+
